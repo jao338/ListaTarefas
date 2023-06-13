ca331463dde1c67f566bbafe2d00c6b29abee7c9
Jão <joaohenriquerc123@gmail.com>
2023-06-12 21:25:41 -0300

    Criação da Navbar e persolinalização

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tarefas</title>
+
+    <!--Links-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img-icon/16x16.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    
+    <nav class="navbar fixed-top bg-body-tertiary">
+
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="img-icon/256x256.png" alt="agenda" width="56" height="56" class="logo-header d-inline-block align-text-center"><p id="texto-header">Lista de Tarefas</p>
+            </a>
+
+            <button type="button" class="btn btn-primary">Login</button>
+        </div>
+
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+    
